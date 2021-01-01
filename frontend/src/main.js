bd0064dfe84109fad65fcb231ae55ffa93cb0cbf
Victor <deep-research>
2020-12-31 21:04:30 -0600

    Bootstrap Installed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,17 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  
+  // Make BootstrapVue available throughout your project
+  Vue.use(BootstrapVue)
+  // Optionally install the BootstrapVue icon components plugin
+  Vue.use(IconsPlugin)
 }
