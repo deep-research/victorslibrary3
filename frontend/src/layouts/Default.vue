1760adbf437b3c6b27c47abc107fe3f2469cffa1
Victor <deep-research>
2021-01-04 02:53:30 -0600

    Bootstrap Editing Test

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -1,36 +1,26 @@
 <template>
-  <div>
-    <b-navbar toggleable="sm" type="light" variant="light">
-      <b-container>
-        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+<div>
+  <b-navbar toggleable="md" type="light" variant="light">
+    <b-container>
+      <b-navbar-brand href="#" tag="h1" class="mb-0">Victor's Library</b-navbar-brand>
 
-        <!-- Website Title -->
-        <b-navbar-brand tag="h1" class="mb-0">
-          <span class="site-title"><g-link to="/">{{ $static.metadata.siteName }}</g-link></span><br />
-          <span class="site-subtitle">&#8239;Research on Music
-            <!-- Responsive subtitle text wrapping -->
-            <span class="site-subtitle-xs"><br />&#8239;</span>and 
-            <span class="site-subtitle-sm"><br />&#8239;</span>Religion
-          </span>
-        </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <!-- Navbar Navigation Menu -->
-        <b-collapse id="nav-text-collapse" is-nav>
-          <b-navbar-nav id="navbar-nav" class="ml-auto">
-            <b-nav-item href="/">
-              <!-- House icon -->
-              <b-icon-house-fill class="navbar-nav-icon" />Home</b-nav-item>
-            <b-nav-item href="/music">
-              <!-- Music Note icon -->
-              <b-icon-music-note-beamed class="navbar-nav-icon" />Music</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-container>
-    </b-navbar>
-    <hr class="mt-0" />
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-text>Research on Music and Religion</b-nav-text>
+        </b-navbar-nav>
 
-    <slot/>
-  </div>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">Music</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+  <hr class="mt-0" />
+</div>
 </template>
 
 <static-query>
@@ -43,101 +33,7 @@ query {
 </static-query>
 
 <style>
-  /* Remove list Indentation */
-  ul, ol {
-    padding-left: 20px;
-  }
-
-  /* Google Font for Title */
-  .site-title {
-    font-family: 'Merienda One', Merriweather, lato,'helvetica neue', helvetica, arial, serif;
-    font-size: 38px;
-  }
-
-  /* Remove Title link formatting */
-  .site-title a, .site-title a:hover {
-    text-decoration: none;
-    color: black;
- }
-
-  /* Recreate Navbar Nav link formatting */
-  /* #navbar-nav a {
-    text-decoration: none;
-    color:rgba(0, 0, 0, 0.5)
-  }
-  #navbar-nav a:hover {
-    color: rgba(0, 0, 0, 0.7)
-  } */
-
-  /* Change Navbar color and background image. */
-  /* .navbar.navbar-light.bg-light{
-    background-color: blue !important;
-    background-image: url("");
-    padding-top: 0;
-    margin-bottom: 0;
-  } */
-
-  /* Responsive Title font size */
-  @media only screen and (max-width: 420px) {
-    .site-title {
-      font-size: 27px;
-      transition: .1s linear;
-    }    
-  }
-  @media only screen and (max-width: 319px) {
-    .site-title {
-      font-size: 23px;
-      transition: .1s linear;
-    }    
-  }  
-
-  /* Google Font for Subtitle */
-  .site-subtitle {
-    font-family: 'Gilda Display', lato,'helvetica neue', helvetica, arial, sans-serif;
-    font-size: 20px;
-    font-weight: bold;
-    white-space: normal
-  }
-
-  /* Google Font for body text */
-  p, ul, ol {
-    line-height: 175%;
-    font-size: 17.25px;
-    font-family: 'Amiko','lato', lato, 'helvetica neue', helvetica, arial, sans-serif;
-  }
-
-  /* Responsive Subtitle font size */
-  @media only screen and (max-width: 420px) {
-    .site-subtitle {
-      font-size: 16px;
-      transition: .1s linear;
-    }
-  }
-
-  /* Responsive subtitle text wrapping */
-  .site-subtitle-sm {
-    display: none
-  }
-  .site-subtitle-xs {
-    display: none
-  }
-  @media only screen and (min-width: 335px) and (max-width: 360px) {
-    .site-subtitle-sm {
-      display: inline;
-    }
-  }
-  @media only screen and (max-width: 335px) {
-    .site-subtitle-xs {
-      display: inline;
-    }
-    /* No Brand margin on mobile */
-    .navbar-brand {
-      margin-right: 0px !important;
-    }
-  }
-
-  /* Vertically center Navbar Nav icons */
-  .navbar-nav-icon {
-    padding-bottom:5.5px
-  }
+.navbar-nav .navbar-text {
+    color: rgba(0, 0, 0, 0.9);
+}
 </style>
---
+++ frontend/src/old/Index Page.vue
@@ -0,0 +1,116 @@
+<template>
+  <Layout>
+    <b-container>
+      <b-row align-h="center">
+        <b-col cols="10" class="welcome-quote">
+          <p class="mb-0">"The individual has always had to struggle to keep from being overwhelmed by the tribe.
+            If you try it, you will be lonely often, and sometimes frightened. But no price is too
+            high to pay for the privilege of owning yourself."</p>
+          <p class="text-center">- Rudyard Kipling</p>          
+        </b-col>
+      </b-row>
+      <br />
+
+      <div class="page-title-mobile">
+        <h3 class="text-center">Home</h3>
+        <br />
+      </div>
+      <b-row class="flex-column-reverse flex-md-row">
+        <b-col md="8">
+          <div class="page-title-desktop">
+            <h3 class="text-center">Home</h3>
+            <br />
+          </div>
+
+          <!-- <h4>News</h4>
+          <ul>
+            <li>1/2/21: The United States Congress will vote on the outcome of the 2020
+              Presidential Election on the 6th of this month. President Trump will speak at a rally
+              contesting the results of the election right before the vote. He alleges that there
+              was widespread fraud in battleground states against his campaign.
+            </li>
+          </ul>
+          <br /> -->
+
+          <h4 id="updates">Updates</h4>
+          <ul>
+            <li>1/2/21: This is an experimental redesign of <a href="https://victorslibrary.com/"
+              target="_blank">victorslibrary.com</a>. The <g-link to="/">Home</g-link> page has
+              been created.</li>
+          </ul>
+          <br />
+
+          <h4 id="topics">Topics</h4>
+          <ul>
+            <li><g-link to="/music">Music</g-link></li>
+          </ul>
+          <br />
+
+          <h4 id="about">About</h4>
+          <p>Victor's library contains multidisciplinary research on rock music and occult philosophy.
+            Victor holds a bachelor’s degree in music and religion, and he is also interested in
+            library science.</p>
+          <p>Thank you for visiting! Questions and comments are welcome.</p>
+          <b-container class="px-0">
+            <b-button href="https://bandcamp.com/contact?b=1631569890"
+              target="_blank">Contact Form</b-button>
+          </b-container>
+          <br />
+          <p style="font-weight: bold;">Copyright © {{ new Date().getFullYear() }}
+            by Victor Fisher</p>
+          <br />
+
+          <g-image style="display:block;" src="~/mushroom.svg" width="200"/>
+          <br /><br />
+        </b-col>
+        <b-col md="4">
+          <b-card title="Contents" class="bg-light contents">
+            <b-card-text>
+              <ol>
+                <li><a href="#updates">Updates</a></li>
+                <li><a href="#topics">Topics</a></li>
+                <li><a href="#about">About</a></li>
+              </ol>
+            </b-card-text>
+          </b-card>
+          <br />
+        </b-col>
+      </b-row>
+    </b-container>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Home'
+  }
+}
+</script>
+
+<style>
+  .welcome-quote p {
+    /* margin-right: 3rem;
+    margin-left: 3rem; */
+    font-size: 100%;
+    line-height: 150%;
+    color: #6c757d;
+  }
+
+  /* Indent Contents List */
+  .contents ol {
+    padding-left: 40px;
+  }
+
+  @media (max-width: 767px) {
+    .page-title-desktop { 
+      display: none;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .page-title-mobile { 
+      display: none;
+    }
+  }
+</style>
