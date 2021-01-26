43dbe63aff63c5ba039a1d89e4e09b56e58b54af
deep-research <fisheve@mx.lakeforest.edu>
2021-01-26 17:56:33 -0600

    404 Page Centered

--- frontend/src/pages/404.vue
+++ frontend/src/pages/404.vue
@@ -1,11 +1,13 @@
 <template>
   <Layout>
-    <b-container class="text-center">
-        <h3 class="mt-4 mb-2">Page Not Found</h3>
-        <h1 class="display-3 mb-4">404</h1>
-        <b-button variant="primary" class="mr-3" g-link to="/">Return to Home</b-button>
-        <b-button variant="secondary" g-link @click="$router.go(-1)">Previous Page</b-button>
-    </b-container>
+    <div class="pb-2 d-flex align-items-center flex-fill">
+      <b-container class="text-center">
+          <h3 class="mb-2">Page Not Found</h3>
+          <h1 class="display-3 mb-4">404</h1>
+          <b-button variant="primary" class="mr-3" g-link to="/">Return to Home</b-button>
+          <b-button variant="secondary" g-link @click="$router.go(-1)">Previous Page</b-button>
+      </b-container>
+    </div>
   </Layout>
 </template>
 
